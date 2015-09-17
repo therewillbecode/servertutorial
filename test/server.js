4da73999e396596144452063e2286d593c1dd3f5
TomChambrier <tomw08@gmail.com>
2015-09-17 14:56:26 +0100

    added first test for server

--- test/server.js
+++ test/server.js
@@ -1,4 +1,17 @@
 /**
  * Created by Tom on 17/09/2015.
  */
+var expect = require('chai').expect;
+var should = require('chai').should();
+var server = require('../server');
+http = require('http');
+
+describe('/', function () {
+    it("root request should respond with status code of 200 ", function (done) {
+        http.get("http://localhost:8888", function (res) {
+            expect(res.statusCode).to.equal(200);
+        done()
+        });
+    });
+});
 
